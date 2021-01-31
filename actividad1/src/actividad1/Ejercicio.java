cb2566c3b5a2d3bdec45eb0f9c8c433db41804d7
SDDAAMM <45061050+SDDAAMM@users.noreply.github.com>
2021-01-31 14:10:37 +0100

    Commit 3

--- actividad1/src/actividad1/Ejercicio.java
+++ actividad1/src/actividad1/Ejercicio.java
@@ -6,6 +6,7 @@ public class Ejercicio {
 		// TODO Auto-generated method stub
 		System.out.println("Hola");
 		System.out.println("Esta es la actividad 1");
+		System.out.println("Realizado por Samuel Jimenez");
 	}
 
 }
